4895b93f0b1c875e062281bee3cf8a1c50d3db80
iXes <quangsexy01@gmail.com>
2021-12-12 02:34:36 +0700

    the open of the flappy game, i hope it will be successfull, this is my first game and im so proud of it

--- game.js
+++ game.js
@@ -1,13 +1,37 @@
 //Select cvs
-const cvs = document.getElementById("bird");
+const cvs = document.getElementById("flappy");
 const ctx = cvs.getContext("2d");
 
 //Game vars and const
 let frames = 0;
+const DEGREE = Math.PI/180;
 
 //Load sprite image
 const sprite = new Image();
 sprite.src = "image/sprite.png";
+
+//Game state
+const state = {
+    current : 0,
+    getReady : 0,
+    game : 1,
+    over : 2
+}
+
+//Controll the game
+document.addEventListener("click", function() {
+    switch (state.current) {
+        case state.getReady:
+            state.current = state.game;
+            break;
+        case state.game:
+            bird.flap();
+            break;                                  
+        case state.over:
+            state.current = state.getReady;
+            break;
+    }
+})
 
 //Background
 const bg = {
@@ -32,10 +56,17 @@ const fg = {
     h : 112,
     x : 0,
     y : cvs.height - 112,
+    dx : 2,
 
     draw : function() {
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    },
+
+    update : function() {
+        if (state.current == state.game) {
+            this.x = (this.x - this.dx)%(this.w/2);
+        }
     }
 }
 
@@ -54,10 +85,53 @@ const bird = {
 
     frame : 0,
 
+    speed : 0,
+    gravity : 0.25,
+    jump : 4.5,
+    rotation : 0,
+
     draw : function() {
         let bird = this.animation[this.frame];
 
-        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x - this.w/2, this.y - this.h/2, this.w, this.h);
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.rotation);
+        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, - this.w/2, - this.h/2, this.w, this.h);
+        ctx.restore();
+    },
+
+    flap : function() {
+        this.speed = - this.jump;
+    },
+
+    update : function() {
+        //If the game state is get ready state, the bird must flap slowly
+        this.period = state.current ==  state.getReady ? 10 : 5;
+        //We increment the frame by 1, each period
+        this.frame += frames%this.period == 0 ? 1 : 0;
+        //Frame goes from 0 to 4, if 4 again to 0
+        this.frame = this.frame%this.animation.length;
+
+        if (state.current == state.getReady) {
+            this.y = 150;
+            this.rotation = 0 * DEGREE;
+        }else {
+            this.speed += this.gravity;
+            this.y += this.speed;
+
+            if (this.y + this.h/2 >= cvs.height - fg.h) {
+                this.y = cvs.height - fg.h - this.h/2;
+                if (state.current == state.game) {
+                    state.current = state.over;
+                }
+            }
+        if (this.speed >= this.jump) {
+            this.rotation = 90 * DEGREE;
+            this.frame = 1;
+        }else {
+            this.rotation = -25 * DEGREE
+        }
+        }
     }
 }
 
@@ -71,7 +145,9 @@ const getReady = {
     y : cvs.height/2 - 152/2,
 
     draw : function() {
+        if (state.current == state.getReady) {
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
     }
 }
 
@@ -85,7 +161,9 @@ const gameOver = {
     y : cvs.height/2 - 202/2,
 
     draw : function() {
+        if (state.current == state.over) {
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
     }
 }
 
@@ -93,6 +171,7 @@ const gameOver = {
 function draw() {
     ctx.fillStyle = "#70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
+
     bg.draw();
     fg.draw();
     bird.draw();
@@ -103,7 +182,8 @@ function draw() {
 
 //Update
 function update() {
-
+    bird.update();
+    fg.update();
 }
 
 //Loop
